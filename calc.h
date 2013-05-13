cb22690e5d3624fa50474bd9cce0296c4bcaa0e3
Andrew414 <andrew414414@gmail.com>
2013-05-13 20:28:06 +0300

    Module prefix defined

--- calc.h
+++ calc.h
@@ -4,3 +4,5 @@ static int open_routine(struct inode *inode, struct file *file);
 static int release_routine(struct inode *inode, struct file *file);
 static ssize_t read_routine(struct file *filp, char *buffer, size_t length, loff_t * offset);
 static ssize_t write_routine(struct file *filp, const char *buff, size_t len, loff_t * off);
+
+#define MODULE_PREFIX "[CALC CDEV] "
