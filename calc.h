ccaf26b885751dcf01d455adf8de1c80f91ebd70
Andrew414 <andrew414414@gmail.com>
2013-05-13 20:19:54 +0300

    Names changed in calc.h

--- calc.h
+++ calc.h
@@ -1,6 +1,6 @@
 #include <linux/fs.h>
 
-static int device_open(struct inode *inode, struct file *file);
-static int device_release(struct inode *inode, struct file *file);
-static ssize_t device_read(  struct file *filp, char *buffer, size_t length, loff_t * offset);
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off);
+static int open_routine(struct inode *inode, struct file *file);
+static int release_routine(struct inode *inode, struct file *file);
+static ssize_t read_routine(struct file *filp, char *buffer, size_t length, loff_t * offset);
+static ssize_t write_routine(struct file *filp, const char *buff, size_t len, loff_t * off);
